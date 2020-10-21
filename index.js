329ce0ec77be572ace1c623c42f808703f3f32e3
dhn149 <marcocamici1986@gmail.com>
2020-10-21 12:40:13 +0200

    Added a selectElement function

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log()
+function selectElement(selector) {
+    const tags = ["h1", "h2", "p"];
+    if (tags.includes(selector));{
+
+return document.getElementsByTagName(selector)[0];
+}
+return document.getElementById(selector);
+}
